c532964487af1629617e90888d833e3e39575f68
snus4me <mathias.hun@gmail.com>
2025-03-19 18:46:16 +0100

    fixed mobile layout to fix sizes to prevent jumping screen

--- src/components/SlideShowElement.jsx
+++ src/components/SlideShowElement.jsx
@@ -26,9 +26,9 @@ const SlideShowElement = ({ cardData, imageLoaded, setImageLoaded }) => {
     }
   };
 
-  if (!imageLoaded) {
-    return null;
-  }
+  // if (!imageLoaded) {
+  //   return null;
+  // }
 
   return (
     <motion.div
@@ -38,41 +38,47 @@ const SlideShowElement = ({ cardData, imageLoaded, setImageLoaded }) => {
       animate={{ opacity: 1 }}
       transition={{ duration: 0.6 }}
     >
-      <section className="slideshow-content__image-section">
-        <picture className="slideshow-content__hero-picture">
-          <source
-            srcSet={images.hero.small.slice(1)}
-            media={`(max-width: 35rem)`}
-          />
-          <source
-            srcSet={images.hero.large.slice(1)}
-            media={`(min-width: 35rem)`}
-          />
-          <img src={images.hero.large.slice(1)} alt="" />
-          <button className="view-image-button" onClick={showModal}>
-            <img className="view-logo" src={viewSVG} alt="" />
-            <span className="uppercase">View image</span>
-          </button>
-        </picture>
-
-        <div className="slideshow-content__meta">
-          <div className="content__heading-cont">
-            <h1 className="text-heading-1">{name}</h1>
-            <p className="text-subhead-1 text-gray">{artist.name}</p>
-            <picture className="content__artist-image--mobile">
-              <img src={artist.image.slice(1)} alt="" />
+      {imageLoaded && (
+        <>
+          <section className="slideshow-content__image-section">
+            <picture className="slideshow-content__hero-picture">
+              <source
+                srcSet={images.hero.small.slice(1)}
+                media={`(max-width: 35rem)`}
+              />
+              <source
+                srcSet={images.hero.large.slice(1)}
+                media={`(min-width: 35rem)`}
+              />
+              <img src={images.hero.large.slice(1)} alt="" />
+              <button className="view-image-button" onClick={showModal}>
+                <img className="view-logo" src={viewSVG} alt="" />
+                <span className="uppercase">View image</span>
+              </button>
             </picture>
-          </div>
-          <picture className="content__artist-image--desktop">
-            <img src={artist.image.slice(1)} alt="" />
-          </picture>
-        </div>
-      </section>
 
-      <section className="content__text-section">
-        <p className="big-text text-display ">{year}</p>
-        <p className="description-text text-body text-gray">{description}</p>
-      </section>
+            <div className="slideshow-content__meta">
+              <div className="content__heading-cont">
+                <h1 className="text-heading-1">{name}</h1>
+                <p className="text-subhead-1 text-gray">{artist.name}</p>
+                <picture className="content__artist-image--mobile">
+                  <img src={artist.image.slice(1)} alt="" />
+                </picture>
+              </div>
+              <picture className="content__artist-image--desktop">
+                <img src={artist.image.slice(1)} alt="" />
+              </picture>
+            </div>
+          </section>
+
+          <section className="content__text-section">
+            <p className="big-text text-display ">{year}</p>
+            <p className="description-text text-body text-gray">
+              {description}
+            </p>
+          </section>
+        </>
+      )}
 
       <dialog className="dialog" ref={dialogRef}>
         <img src={images.gallery.slice(1)} alt="" />
